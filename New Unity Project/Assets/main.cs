191a28667371888d7eba88e30dd341dff610f47b
Kelmatou <antoine.clop@epita.fr>
2015-02-26 16:49:25 +0100

    Ennemie ajouté + mode pause

--- New Unity Project/Assets/main.cs
+++ New Unity Project/Assets/main.cs
@@ -1,20 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class main : MonoBehaviour {
+public class main : MonoBehaviour 
+{
 
 	// Use this for initialization
 	void Start () 
     {
-	
+        GameObject text = GameObject.Find("Display Text");
+        text.guiText.text = "Game";
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	    if(Input.GetKey(KeyCode.Escape))
+        controler_manager();
+	}
+
+    void controler_manager()
+    {
+        if (Input.GetKey(KeyCode.Escape))
         {
             Application.Quit();
         }
-	}
+        if (Input.GetKey(KeyCode.P))
+        {
+            GameObject text = GameObject.Find("Display Text");
+            if(text.guiText.text != "Pause")
+            {
+                pause(text);
+            }
+            else
+            {
+                unpause(text);
+            }
+        }
+    }
+
+    void pause(GameObject text)
+    {
+        text.guiText.text = "Pause";
+    }
+
+    void unpause(GameObject text)
+    {
+        text.guiText.text = "Game";
+    }
 }
